87e0d789e55a83af1afc0b308c692d01ec372b67
Jordi_Torrents <jordi.torrentsm@gmail.com>
2022-04-08 15:01:41 +0200

    Optimizing cpp, even more

--- main.cpp
+++ main.cpp
@@ -22,8 +22,9 @@ private:
     float v0, inv_2L, th = 1.0, rand_const = (1.0 / 4294967296.0);
     const int L, N_cells, N;
     // float *x, *y, *vx, *vy;
-    vector<float> x, y;
-    vector<float> vx, vy;
+    // vector<float> x, y;
+    // vector<vector<float>> vel;, pos;
+    float *pos, *vel;
     int *near_nbr_cells;
     sfmt_t sfmt;
 
@@ -32,17 +33,17 @@ private:
         return value - int(inv_2L * value) * L;
     }
 
-    inline float dist_PBC(float x1, float y1, float x2, float y2)
+    inline float dist_PBC(float *pos1, float *pos2)
     {
-        float dx = pbc(x1 - x2);
-        float dy = pbc(y1 - y2);
+        float dx = pbc(*(pos1++) - *(pos2++));
+        float dy = pbc(*pos1 - *pos2);
         return dx * dx + dy * dy;
     }
 
-    inline float dist_simple(float x1, float y1, float x2, float y2)
+    inline float dist_simple(float *pos1, float *pos2)
     {
-        float dx = x1 - x2;
-        float dy = y1 - y2;
+        float dx = *(pos1++) - *(pos2++);
+        float dy = *pos1 - *pos2;
         return dx * dx + dy * dy;
     }
 
@@ -76,15 +77,18 @@ public:
                               N(int((L_in * L_in * rho_in + 2.0) / 4) * 4),
                               inv_2L(2.0 / float(L_in))
     {
-        // x = (float *)malloc(N * sizeof(float));
-        // y = (float *)malloc(N * sizeof(float));
-        // vx = (float *)malloc(N * sizeof(float));
-        // vy = (float *)malloc(N * sizeof(float));
+        pos = (float *)malloc(2 * N * sizeof(float));
+        vel = (float *)malloc(2 * N * sizeof(float));
 
-        x.resize(N);
-        y.resize(N);
-        vx.resize(N);
-        vy.resize(N);
+        // vel.resize(N);
+        // for (int i = 0; i < N; i++)
+        //     vel[i].resize(2);
+
+        // pos.resize(N);
+        // for (int i = 0; i < N; i++)
+        //     pos[i].resize(2);
+
+        // vy.resize(N);
         // cout << sfmt_get_min_array_size32(&sfmt) << endl;
         near_nbr_cells = (int *)malloc(N_cells * 4 * sizeof(int));
         sfmt_init_gen_rand(&sfmt, seed);
@@ -100,6 +104,8 @@ public:
         // free(y);
         // free(vx);
         // free(vy);
+        free(pos);
+        free(vel);
         free(near_nbr_cells);
     }
 
@@ -107,8 +113,16 @@ public:
     {
         // float sum_x = accumulate(vx, vx + N, 0.0);
         // float sum_y = accumulate(vy, vy + N, 0.0);
-        float sum_x = accumulate(begin(vx), end(vx), 0.0);
-        float sum_y = accumulate(begin(vy), end(vy), 0.0);
+
+        float sum_x = 0, sum_y = 0;
+
+        for (int i = 0; i < 2 * N;)
+        {
+            sum_x += vel[i++];
+            sum_y += vel[i++];
+        }
+        // float sum_x = accumulate(begin(vx), end(vx), 0.0);
+        // float sum_y = accumulate(begin(vy), end(vy), 0.0);
         float polar_i_sq = sum_x * sum_x + sum_y * sum_y;
 
         sum_phis += sqrt(polar_i_sq);
@@ -162,149 +176,145 @@ public:
 
         uint32_t random[random_lenght];
 
-        // uint32_t *random;
-        // if (posix_memalign((void **)&random, 16, sizeof(float) * random_lenght) != 0)
-        //     printf("can't allocate memory.\n");
-
         sfmt_fill_array32(&sfmt, random, random_lenght);
-        float norm = 1.0 / 4294967296.0;
         int rand_indx = 0;
         for (int i = 0; i < N; i++)
         {
-            x[i] = norm * random[rand_indx++] * L;
-            y[i] = norm * random[rand_indx++] * L;
-            float theta = norm * random[rand_indx++] * 6.283185307;
-            vx[i] = cos(theta);
-            vy[i] = sin(theta);
-        }
-
-        // free(random);
-    }
-
-    void neighbours_direction(float *nbr_direction, uint32_t *random)
-    {
-
-        vector<float> integr_vx(vx), integr_vy(vy);
-        vector<int> header(N_cells, -1), cell_list(N);
-
-        // copy(vx.begin(), vx.end(), integr_vx);
-        // copy(vx.begin(), vx.end(), integr_vx);
-        // fill(header.begin(), header.end(), -1);
-
-        for (int i = 0; i < N; i++)
-        {
-            int cell = int(x[i]) % L + L * (int(y[i]) % L);
-            cell_list[i] = header[cell];
-            header[cell] = i;
-            // integr_vx[i] = vx[i];
-            // integr_vy[i] = vy[i];
-        }
-
-        for (int cell = 0; cell < N_cells; cell++)
-        {
-            int *nbr_indx = near_nbr_cells + cell * 4;
-            for (int i = header[cell]; i > -1; i = cell_list[i])
-            {
-
-                for (int j = cell_list[i]; j > -1; j = cell_list[j])
-                {
-                    if (dist_simple(x[i], y[i], x[j], y[j]) < th)
-                    {
-                        integr_vx[i] += vx[j];
-                        integr_vy[i] += vy[j];
-                        integr_vx[j] += vx[i];
-                        integr_vy[j] += vy[i];
-                    }
-                }
-
-                for (int j = header[*nbr_indx++]; j > -1; j = cell_list[j])
-                {
-                    if (dist_PBC(x[i], y[i], x[j], y[j]) < th)
-                    {
-                        integr_vx[i] += vx[j];
-                        integr_vy[i] += vy[j];
-                        integr_vx[j] += vx[i];
-                        integr_vy[j] += vy[i];
-                    }
-                }
-
-                for (int j = header[*nbr_indx++]; j > -1; j = cell_list[j])
-                {
-                    if (dist_PBC(x[i], y[i], x[j], y[j]) < th)
-                    {
-                        integr_vx[i] += vx[j];
-                        integr_vy[i] += vy[j];
-                        integr_vx[j] += vx[i];
-                        integr_vy[j] += vy[i];
-                    }
-                }
-
-                for (int j = header[*nbr_indx++]; j > -1; j = cell_list[j])
-                {
-                    if (dist_PBC(x[i], y[i], x[j], y[j]) < th)
-                    {
-                        integr_vx[i] += vx[j];
-                        integr_vy[i] += vy[j];
-                        integr_vx[j] += vx[i];
-                        integr_vy[j] += vy[i];
-                    }
-                }
-
-                for (int j = header[*nbr_indx]; j > -1; j = cell_list[j])
-                {
-                    if (dist_PBC(x[i], y[i], x[j], y[j]) < th)
-                    {
-                        integr_vx[i] += vx[j];
-                        integr_vy[i] += vy[j];
-                        integr_vx[j] += vx[i];
-                        integr_vy[j] += vy[i];
-                    }
-                }
-                nbr_indx -= 3;
-            }
-        }
-
-        // float random[N];
-        // for (int i = 0; i < N; i++)
-        // {
-        //     random[i] = RAN;
-        // }
-        // float factor1 = eta * 6.283185307179586;
-        // float factor2 = -eta * 3.14159265359;
-
-        // uint32_t random[N];
-        // sfmt_fill_array32(&sfmt, random, N);
-        float factor1 = eta * 6.283185307179586 / 4294967296.0;
-        float factor2 = -eta * 3.14159265359;
-        // cout << random[0] * (1 / 4294967296.0) << endl;
-        for (int i = 0; i < N; i++)
-        {
-            nbr_direction[i] = atan2(integr_vy[i], integr_vx[i]) + factor1 * random[i] + factor2;
+            pos[2 * i] = rand_const * random[rand_indx++] * L;
+            pos[2 * i + 1] = rand_const * random[rand_indx++] * L;
+            float theta = rand_const * random[rand_indx++] * 6.283185307;
+            vel[2 * i] = cos(theta);
+            vel[2 * i + 1] = sin(theta);
         }
     }
 
     void integrate(int steps = 1, int update_obs = 0)
     {
+
+        // vector<vector<float, 2>, N> integr;
+        float *integr = (float *)malloc(2 * N * sizeof(float));
+        int *header = (int *)malloc(N_cells * sizeof(int));
+        int *cell_list = (int *)malloc(2 * N * sizeof(int));
+
+        // float integr[2 * N];
+        // int header[N_cells];
+        // int cell_list[2 * N];
+        // vector<float> integr(2 * N);
+        // vector<int> header(N_cells);
+        // vector<int> cell_list(2 * N);
+
         float particle_direction[N];
         int random_lenght = max(N, sfmt_get_min_array_size32(&sfmt));
-        uint32_t random[random_lenght];
+        uint32_t *random = (uint32_t *)malloc(random_lenght * sizeof(uint32_t));
+        float factor1 = eta * 6.283185307179586 * rand_const;
+        float factor2 = -eta * 3.14159265359;
+
+        // TODO: Create an array of pointers do every Y element (another for X) of integr (or vel os pos...)
+        // Objective. Clean up the code without (2*i + 1) while having contiguous x/y components.
 
         for (int step = 0; step < steps; step++)
         {
+            for (int i = 0; i < N_cells; i++)
+                header[i] = -1;
+
+            for (int i = 0; i < 2 * N; i++)
+                integr[i] = vel[i];
+
+            for (int i = 0; i < 2 * N; i += 2)
+            {
+                int cell = int(pos[i]) % L + L * (int(pos[i + 1]) % L);
+                cell_list[i] = header[cell];
+                header[cell] = i;
+            }
+
+            for (int cell = 0; cell < N_cells; cell++)
+            {
+                int *nbr_indx = near_nbr_cells + cell * 4;
+                for (int i = header[cell]; i > -1; i = cell_list[i])
+                {
+
+                    for (int j = cell_list[i]; j > -1; j = cell_list[j])
+                    {
+                        if (dist_simple(&pos[i], &pos[j]) < th)
+                        {
+                            integr[i] += vel[j];
+                            integr[i + 1] += vel[j + 1];
+                            integr[j] += vel[i];
+                            integr[j + 1] += vel[i + 1];
+                        }
+                    }
+
+                    for (int j = header[*nbr_indx++]; j > -1; j = cell_list[j])
+                    {
+                        if (dist_PBC(&pos[i], &pos[j]) < th)
+                        {
+                            integr[i] += vel[j];
+                            integr[i + 1] += vel[j + 1];
+                            integr[j] += vel[i];
+                            integr[j + 1] += vel[i + 1];
+                        }
+                    }
+
+                    for (int j = header[*nbr_indx++]; j > -1; j = cell_list[j])
+                    {
+                        if (dist_PBC(&pos[i], &pos[j]) < th)
+                        {
+                            integr[i] += vel[j];
+                            integr[i + 1] += vel[j + 1];
+                            integr[j] += vel[i];
+                            integr[j + 1] += vel[i + 1];
+                        }
+                    }
+
+                    for (int j = header[*nbr_indx++]; j > -1; j = cell_list[j])
+                    {
+                        if (dist_PBC(&pos[i], &pos[j]) < th)
+                        {
+                            integr[i] += vel[j];
+                            integr[i + 1] += vel[j + 1];
+                            integr[j] += vel[i];
+                            integr[j + 1] += vel[i + 1];
+                        }
+                    }
+
+                    for (int j = header[*nbr_indx]; j > -1; j = cell_list[j])
+                    {
+                        if (dist_PBC(&pos[i], &pos[j]) < th)
+                        {
+                            integr[i] += vel[j];
+                            integr[i + 1] += vel[j + 1];
+                            integr[j] += vel[i];
+                            integr[j + 1] += vel[i + 1];
+                        }
+                    }
+                    nbr_indx -= 3;
+                }
+            }
+
             sfmt_fill_array32(&sfmt, random, random_lenght);
-            neighbours_direction(particle_direction, random);
+
+            for (int i = 0; i < N; i++)
+
+                particle_direction[i] = atan2(integr[2 * i + 1], integr[2 * i]) + factor1 * random[i] + factor2;
 
             for (int i = 0; i < N; i++)
             {
-                vx[i] = cos(particle_direction[i]);
-                vy[i] = sin(particle_direction[i]);
-                x[i] = wrap3(x[i] + v0 * vx[i]);
-                y[i] = wrap3(y[i] + v0 * vy[i]);
+                vel[2 * i] = cos(particle_direction[i]);
+                vel[2 * i + 1] = sin(particle_direction[i]);
+            }
+
+            for (int i = 0; i < 2 * N; i++)
+            {
+                pos[i] = wrap3(pos[i] + v0 * vel[i]);
             }
 
             if (update_obs)
                 update_observables();
         }
+        free(header);
+        free(cell_list);
+        free(integr);
+        free(random);
     }
 };
 
